5dd6c17dc4f05edb8ece1c88006aa64a7be43fb2
Khalif AL Mahmud <khalif@Khalifs-MBP.netis>
2023-09-01 21:58:14 +0600

    Fix Mobile Issue

--- src/pages/Layout.svelte
+++ src/pages/Layout.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <section>
-	<div class="grid grid-rows-6 grid-cols-4 md:grid-cols-12 h-96 grid-flow-col">
+	<!-- <div class="grid grid-rows-6 md:grid-cols-12 h-96 grid-flow-col">
 		<div class="bg-blue-400 md:col-span-2 md:row-span-3 md:order-1" />
 		<div class="bg-teal-400 md:col-span-2 md:row-span-2 md:order-2" />
 		<div class="bg-fuchsia-400 md:col-span-6 md:row-span-1 md:order-3" />
@@ -10,6 +10,45 @@
 		<div class="bg-blue-500 md:col-span-6 md:row-span-2 md:order-5" />
 		<div class="bg-yellow-100 md:col-span-4 md:row-span-4 md:order-6" />
 		<div class="bg-emerald-400 md:col-span-2 md:row-span-4 md:order-7" />
+	</div> -->
+	<div
+		class="grid grid-rows-12 md:grid-rows-6 grid-cols-4 md:grid-cols-12 h-screen md:h-96 grid-flow-row md:grid-flow-col"
+	>
+		<div
+			class="bg-blue-400 text-center col-span-4 row-span-3 order-1 md:col-span-2 md:row-span-3 md:order-1"
+		>
+			1
+		</div>
+		<div
+			class="bg-teal-400 text-center col-span-4 row-span-3 order-2 md:col-span-2 md:row-span-2 md:order-2"
+		>
+			2
+		</div>
+		<div
+			class="bg-fuchsia-400 text-center col-span-4 row-span-3 order-5 md:col-span-6 md:row-span-1 md:order-3"
+		>
+			3
+		</div>
+		<div
+			class="bg-rose-100 text-center col-span-4 row-span-3 order-3 md:col-span-4 md:row-span-5 md:order-4"
+		>
+			4
+		</div>
+		<div
+			class="bg-blue-500 text-center col-span-4 row-span-3 order-6 md:col-span-6 md:row-span-2 md:order-5"
+		>
+			5
+		</div>
+		<div
+			class="bg-yellow-100 text-center col-span-4 row-span-3 order-4 md:col-span-4 md:row-span-4 md:order-6"
+		>
+			6
+		</div>
+		<div
+			class="bg-emerald-400 text-center col-span-4 row-span-3 order-7 md:col-span-2 md:row-span-4 md:order-7"
+		>
+			7
+		</div>
 	</div>
 </section>
 
