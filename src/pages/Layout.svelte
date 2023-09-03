e5baad43ad5c4cd9611eb2b135e0e33310bd7ebb
Khalif AL Mahmud <khalif@Khalifs-MBP.netis>
2023-09-03 20:55:45 +0600

    fix mountain

--- src/pages/Layout.svelte
+++ src/pages/Layout.svelte
@@ -7,45 +7,64 @@
 	>
 		<div
 			class="bg-blue-400 text-center col-span-4 row-span-3 order-1 md:col-span-2 md:row-span-3 md:order-1"
-		>
-			1
-		</div>
+		/>
 		<div
 			class="bg-teal-400 text-center col-span-4 row-span-3 order-2 md:col-span-2 md:row-span-2 md:order-2"
-		>
-			2
-		</div>
+		/>
 		<div
 			class="bg-fuchsia-400 text-center col-span-4 row-span-3 order-5 md:col-span-6 md:row-span-1 md:order-3"
-		>
-			3
-		</div>
+		/>
 		<div
 			class="bg-rose-100 text-center col-span-4 row-span-3 order-3 md:col-span-4 md:row-span-5 md:order-4"
-		>
-			4
-		</div>
+		/>
 		<div
 			class="bg-blue-500 text-center col-span-4 row-span-3 order-6 md:col-span-6 md:row-span-2 md:order-5"
-		>
-			5
-		</div>
+		/>
 		<div
 			class="bg-yellow-100 text-center grid justify-center content-center col-span-4 row-span-3 order-4 md:col-span-4 md:row-span-4 md:order-6"
 		>
 			<div
-				class="mountain grid justify-center content-center w-[300px] h-[300px] m-16 bg-[#293462]"
+				class=" grid justify-center content-center w-[300px] h-[300px] m-16 bg-[#293462]"
 			>
-				<div class="w-[200px] h-[200px] bg-yellow-100 rounded-full">a</div>
+				<div
+					class="mountain_circle w-[200px] h-[200px] bg-yellow-100 rounded-full"
+				>
+					<div class="main_mountain1" />
+					<div class="main_mountain2" />
+				</div>
 			</div>
 		</div>
 		<div
 			class="bg-emerald-400 text-center col-span-4 row-span-3 order-7 md:col-span-2 md:row-span-4 md:order-7"
-		>
-			7
-		</div>
+		/>
 	</div>
 </section>
 
 <style>
+	.mountain_circle {
+		position: relative;
+		overflow: hidden;
+	}
+	.main_mountain1 {
+		position: absolute;
+		content: "";
+		bottom: 0px;
+		left: -40px;
+		border-top: 0px solid transparent;
+		border-right: 70px solid transparent;
+		border-bottom: 70px solid #fe5f55;
+		border-left: 70px solid transparent;
+		z-index: 5;
+	}
+	.main_mountain2 {
+		position: absolute;
+		content: "";
+		bottom: 0px;
+		left: 10px;
+		border-top: 0px solid transparent;
+		border-left: 130px solid transparent;
+		border-bottom: 130px solid #fe5f55;
+		border-right: 120px solid transparent;
+		z-index: 5;
+	}
 </style>
